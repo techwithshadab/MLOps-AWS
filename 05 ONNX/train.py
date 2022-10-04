57164f933fd8216d17e85a4d7fbcc8741c8c2ee0
techwithshadab <shadab.entrepreneur@outlook.com>
2022-10-04 10:09:26 +0530

    mlops

--- 05 ONNX/train.py
+++ 05 ONNX/train.py
@@ -1,13 +1,19 @@
 import torch
+import hydra
 import wandb
+import logging
+
 import pandas as pd
 import pytorch_lightning as pl
+from omegaconf.omegaconf import OmegaConf
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
 
 from data import DataModule
 from model import ColaModel
+
+logger = logging.getLogger(__name__)
 
 
 class SamplesVisualisationLogger(pl.Callback):
@@ -37,13 +43,20 @@ class SamplesVisualisationLogger(pl.Callback):
         )
 
 
-def main():
-    cola_data = DataModule()
-    cola_model = ColaModel()
+@hydra.main(config_path="./configs", config_name="config")
+def main(cfg):
+    logger.info(OmegaConf.to_yaml(cfg, resolve=True))
+    logger.info(f"Using the model: {cfg.model.name}")
+    logger.info(f"Using the tokenizer: {cfg.model.tokenizer}")
+    cola_data = DataModule(
+        cfg.model.tokenizer, cfg.processing.batch_size, cfg.processing.max_length
+    )
+    cola_model = ColaModel(cfg.model.name)
 
+    root_dir = hydra.utils.get_original_cwd()
     checkpoint_callback = ModelCheckpoint(
-        dirpath="./models",
-        filename="best-checkpoint.ckpt",
+        dirpath=f"{root_dir}/models",
+        filename="best-checkpoint",
         monitor="valid/loss",
         mode="min",
     )
@@ -54,15 +67,16 @@ def main():
 
     wandb_logger = WandbLogger(project="MLOPS AWS", entity="techwithshadab")
     trainer = pl.Trainer(
-        max_epochs=1,
+        max_epochs=cfg.training.max_epochs,
         logger=wandb_logger,
         callbacks=[checkpoint_callback, SamplesVisualisationLogger(cola_data), early_stopping_callback],
-        log_every_n_steps=10,
-        deterministic=True,
-        # limit_train_batches=0.25,
-        # limit_val_batches=0.25
+        log_every_n_steps=cfg.training.log_every_n_steps,
+        deterministic=cfg.training.deterministic,
+        # limit_train_batches=cfg.training.limit_train_batches,
+        # limit_val_batches=cfg.training.limit_val_batches,
     )
     trainer.fit(cola_model, cola_data)
+    wandb.finish()
 
 
 if __name__ == "__main__":
