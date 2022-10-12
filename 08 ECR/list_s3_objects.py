0f3198e354a6d9385cfe8eb7a8ebca5325582442
techwithshadab <shadab.entrepreneur@outlook.com>
2022-10-12 15:27:17 +0530

    push docker to ecr

--- 08 ECR/list_s3_objects.py
+++ 08 ECR/list_s3_objects.py
@@ -1,5 +1,8 @@
-import boto3
-s3 = boto3.resource("s3")
-bucket = s3.Bucket("mlops-aws")
-for each in bucket.objects.all():
-    print(each.key)
+# import boto3
+# s3 = boto3.resource("s3")
+# bucket = s3.Bucket("mlops-aws")
+# for each in bucket.objects.all():
+#     print(each.key)
+import os
+print(os.environ["AWS_ACCESS_KEY_ID"])
+print(os.environ["AWS_SECRET_ACCESS_KEY"])
